02347de5f531be32bfc43e436cdba097a04c03ed
Aleksey Vinnik <vinniko333@gmail.com>
2021-07-22 14:57:04 +0300

    vn: create option_product migration commit

---
+++ database/migrations/2021_07_22_113621_create_option_product_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOptionProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('options_products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('option_id')->constrained();
+            $table->foreignId('product_id')->constrained();
+            $table->string('value');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('options_products');
+    }
+}
